018883395c790c74b04303487b3ef62ec5211548
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2025-03-11 10:55:58 +0100

    no print if already set

--- views_stepshifter/manager/__init__.py
+++ views_stepshifter/manager/__init__.py
@@ -5,6 +5,7 @@ try:
     set_start_method('spawn')
     print(f"Multiprocessing start method set to: {get_start_method()}") # Use print because logger is not yet configured
 except RuntimeError:
-    print(f"Multiprocessing start method is already set to: {get_start_method()}") # Use print because logger is not yet configured
+    # print(f"Multiprocessing start method is already set to: {get_start_method()}") 
+    pass
 
 __all__ = ["StepshifterManager"]
