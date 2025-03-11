78cd49eccc4d6c7d3c192e9f2c0af950e2f3bf52
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2025-03-11 10:42:46 +0100

    add __init__.py

---
+++ views_stepshifter/manager/__init__.py
@@ -0,0 +1,10 @@
+from multiprocessing import set_start_method, get_start_method
+from views_stepshifter.manager.stepshifter_manager import StepshifterManager
+
+try:
+    set_start_method('spawn')
+    print(f"Multiprocessing start method set to: {get_start_method()}") # Use print because logger is not yet configured
+except RuntimeError:
+    print(f"Multiprocessing start method is already set to: {get_start_method()}") # Use print because logger is not yet configured
+
+__all__ = ["StepshifterManager"]
